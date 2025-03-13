43ce97cb75548ead18185f1c325b8c9fa49be647
dmiv <dmitriy40647274@gmail.com>
2025-03-13 09:46:36 +0300

    interactor

--- services/test_images.py
+++ services/test_images.py
@@ -1,4 +1,5 @@
 # Test for cover_text function
+from interactors.images import image_instagram_process_interactor
 from services.images import cover_text, process_image, image_to_bytes
 from services.text import wrap_text, calculate_characters_width
 
@@ -7,33 +8,34 @@ def test_cover_text():
     # Read test image file as bytes
     with open('img_1.png', 'rb') as f:
         image_bytes = f.read()
+        test_text = (
+            "Это пример текста, который нужно ограничить по ширине в 32 символа. "
+            "Он содержит длинные слова, такие как 'саморазвитие' и 'высокотехнологичный', "
+            "а также короткие слова."
+        )
+        result = image_instagram_process_interactor(image_bytes, test_text, 100)
+#     # Test parameters
+#     # test_text = "Test Text"
 
-    # Test parameters
-    # test_text = "Test Text"
-    test_text = (
-    "Это пример текста, который нужно ограничить по ширине в 32 символа. "
-    "Он содержит длинные слова, такие как 'саморазвитие' и 'высокотехнологичный', "
-    "а также короткие слова."
-)
-    # test_text = "12345hello, world уцацуукцук"
-    test_font_size = 100
-
-    text_width = calculate_characters_width(test_font_size)
-    # output_text = test_text
-    output_text = wrap_text(test_text, width=text_width)
-    # print("Output text is", output_text)
-    image_resized = process_image(image_bytes)
-    # image_bytes_resized = image_resized.tobytes()
-    image_bytes_resized = image_to_bytes(image_resized)
-    # Call cover_text function
-    result = cover_text(image_bytes_resized, output_text, test_font_size)
-
-    # Verify result is bytes
-    assert isinstance(result, bytes)
-
-    # Verify result is not empty
-    assert len(result) > 0
-    # Save result to file
+#     # test_text = "12345hello, world уцацуукцук"
+#     test_font_size = 100
+#
+#     text_width = calculate_characters_width(test_font_size)
+#     # output_text = test_text
+#     output_text = wrap_text(test_text, width=text_width)
+#     # print("Output text is", output_text)
+#     image_resized = process_image(image_bytes)
+#     # image_bytes_resized = image_resized.tobytes()
+#     image_bytes_resized = image_to_bytes(image_resized)
+#     # Call cover_text function
+#     result = cover_text(image_bytes_resized, output_text, test_font_size)
+#
+#     # Verify result is bytes
+#     assert isinstance(result, bytes)
+#
+#     # Verify result is not empty
+#     assert len(result) > 0
+#     # Save result to file
     with open('result.png', 'wb') as f:
         f.write(result)
 
