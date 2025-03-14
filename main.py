ca6316a433e027621a5c938fb845a90b6eb79ab0
dmiv <dmitriy40647274@gmail.com>
2025-03-14 19:06:53 +0300

    1

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-# from aiogram.utils import executor
 from config import BOT_TOKEN
 from router import router
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
 dp = Dispatcher()
 dp.include_router(router)
 
 
-# Инициализация обработчика
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
@@ -26,7 +23,6 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 
-# Запуск бота
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
